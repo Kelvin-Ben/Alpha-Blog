faaaee86e4c908dba83501cdd14e51143a235a23
Kelvin-Ben <[kevoben14@gmail.com]>
2023-02-08 15:37:49 +0300

    Create new template, action and routes

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,15 @@
+class ArticlesController < ApplicationController 
+  def new
+     @article = Article.new
+  end
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_show(@article)
+  end
+
+    private
+      def article_params
+        params.require(:article).permit(:title, :description)
+      end
+end
