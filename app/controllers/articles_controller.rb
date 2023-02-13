b6203b5b154a8ba3f15ee8bfcee51d886e507684
Kelvin-Ben <[kevoben14@gmail.com]>
2023-02-13 20:12:50 +0300

    Fix Create, add flash message, add show action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,28 @@
-class ArticlesController < ApplicationController 
+class ArticlesController < ApplicationController
+  # before_action :set_article, only: [:edit, :update, :show, :destroy]
+  # def index
+  #   @article = Article.all
+  # end
   def new
-     @article = Article.new
-  end
-  def create
-    @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    @article = Article.new
   end
 
-    private
-      def article_params
-        params.require(:article).permit(:title, :description)
-      end
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+  
+  private
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
 end
